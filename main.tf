1983a45527258ac1911b1e8c5200e201d180aa15
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-22 01:20:33 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ provider "restapi" {
   write_returns_object = true
   insecure             = true
   id_attribute         = "/"
-//  create_method        = "PUT"
-//  update_method        = "PUT"
-//  destroy_method       = "PUT"
+  create_method        = "PUT"
+  update_method        = "PUT"
+  destroy_method       = "PUT"
 }
 
 resource "restapi_object" "create_repository" {
@@ -27,10 +27,10 @@ resource "restapi_object" "create_repository" {
   data = "{\"type\": \"s3\", \"settings\": {\"client\": \"default\", \"bucket\": \"eck-bucket\", \"base_path\": \"eck-ss/\"}}"
 }
 
-//resource "restapi_object" "take_snapshot" {
-//  depends_on = [restapi_object.create_repository]
-//  object_id = "s3snapshot"
-//  path = "/eck-ss/snapshot-1"
-//  data = "{\"indices\": \"index_1,index_2\",\"ignore_unavailable\": true,\"include_global_state\": false,\"metadata\": {\"taken_by\": \"elastic\",\"taken_because\": \"backup before upgrading\"}}"
-//}
+resource "restapi_object" "take_snapshot" {
+  depends_on = [restapi_object.create_repository]
+  object_id = "s3snapshot"
+  path = "/eck-ss/snapshot-1"
+  data = "{\"indices\": \"index_1,index_2\",\"ignore_unavailable\": true,\"include_global_state\": false,\"metadata\": {\"taken_by\": \"elastic\",\"taken_because\": \"backup before upgrading\"}}"
+}
 
