600dfcb1cb64a25ad07c20e4a9f5cd129274030d
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-22 02:47:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,15 @@
 
+variable "master_uri" {
+  type = string
+  description = "Master URL"
+}
+
+variable "s3_bucket" {
+  type = string
+  description = "S3 bucket name"
+}
+
+variable "base_path" {
+  type = string
+  description = "Base Path"
+}
