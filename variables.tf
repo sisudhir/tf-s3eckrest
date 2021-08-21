56c40924444f5c683c42cd0feddbe4f226dc501a
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-22 03:37:58 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "masteruri" {
+  type = string
+  description = "Master URL"
+}
+
+variable "s3bucket" {
+  type = string
+  description = "S3 bucket name"
+}
+
+variable "basepath" {
+  type = string
+  description = "Base Path"
+}
