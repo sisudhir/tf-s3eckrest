ce8959586fcc794693333857514512d6b34e0d9c
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-22 02:47:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,20 +8,6 @@ terraform {
   }
 }
 
-variable "master_uri" {
-  type = string
-  description = "Master URL"
-}
-
-variable "s3_bucket" {
-  type = string
-  description = "S3 bucket name"
-}
-
-variable "base_path" {
-  type = string
-  description = "Base Path"
-}
 
 provider "restapi" {
   uri                  = "${master_uri}/_snapshot"
