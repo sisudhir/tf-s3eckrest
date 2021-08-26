7b27a96e1452d18046354ec0cc6a93916f3d37eb
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-27 03:52:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,33 +27,3 @@ resource "restapi_object" "create_repository" {
   path = "/eck-ss"
   data = "{\"type\": \"s3\", \"settings\": {\"client\": \"default\", \"bucket\": \"eck-bucket\", \"base_path\": \"eck-ss/\"}}"
 }
-
-provider "restapi" {
-  alias                = "west"
-  uri                  = "http://192.168.1.93:32560/_slm"
-  debug                = true
-  headers              = {"Content-Type" = "application/json"}
-  write_returns_object = true
-  insecure             = true
-  id_attribute         = "/"
-  create_method        = "PUT"
-  update_method        = "PUT"
-  destroy_method       = "PUT"
-}
-
-resource "restapi_object" "create_policy" {
-  provider = restapi.west
-  depends_on = [restapi_object.create_repository]
-  object_id = "sspolicy"
-  path = "/policy/daily-snapshots"
-  data = "\"name\": \"weather-data\", \"snapshotName\": \"weather-data-policy1\", \"schedule\": \"0 0 0 * * ?\", \"repository\": \"eck-ss\", \"config\":{\"indices\": [\"weather-data-2016\"]}, \"retention\": {\"expireAfterUnit\": \"d\"}, \"isManagedPolicy\": false}"
-}
-
-
-resource "restapi_object" "exec_policy" {
-  provider = restapi.west
-  depends_on = [restapi_object.create_policy]
-  object_id = "sspolicyexec"
-  path = "/policy/daily-snapshots/_execute"
-  data = ""
-}
