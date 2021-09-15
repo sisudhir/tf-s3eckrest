a8d9898d9c5f1abcedaa59f1f48896f1bef385f9
Sindhu Sudhir <sisudhir@cisco.com>
2021-09-15 07:25:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,14 @@
 variable "eckmstr_uri" {
   type = string
-  description = "Elastic master ip and port"
-  default = "192.168.1.93:32560"
+  description = "Elastic master URI"
 }
 
 variable "bucket_name" {
   type = string
   description = "s3 bucket name in AWS"
-  default = "eck-bucket"
 }
 
 variable "base_path" {
   type = string
   description = "ECK snapshot Name"
-  default = "eck-ss"
 }
